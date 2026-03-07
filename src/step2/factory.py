1a05f64fc8bf790a0f52cd5d379628080696a056
Dmitrii Zavalin's Aerospace & IT Projects <135647339+Dmitrii-Zavalin-Deployments@users.noreply.github.com>
2026-03-07 16:07:31 -0700

    Create factory.py

---
+++ src/step2/factory.py
@@ -0,0 +1,11 @@
+from .cell import Cell
+from src.solver_input import SolverInput
+
+class CellBuilder:
+    def __init__(self, input_data: SolverInput):
+        self.input_data = input_data
+
+    def build(self, i: int, j: int, k: int) -> Cell:
+        # Translates flat/structured input into the transient Cell object
+        mask_val = self.input_data.mask.data[i, j, k]
+        return Cell(x=i, y=j, z=k, mask=mask_val)
