3b9fdca6876b0d3324b181d9e2a512f224cb3980
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-03-30 08:34:57 -0600

    testing the prototype

--- .tests_legacy_archive/operators/test_forces.py
+++
@@ -1,9 +0,0 @@
-# tests/operators/test_forces.py
-
-def test_get_body_forces_interior():
-    nx, ny, nz = 5, 5, 5
-    # The output should now be shape (3, nx-2, ny-2, nz-2)
-    forces = get_body_forces_interior(nx, ny, nz, 0.0, -9.81, 0.0)
-    
-    assert forces.shape == (3, 3, 3, 3) 
-    assert np.all(forces[1] == -9.81)
